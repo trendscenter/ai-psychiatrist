75f2fda53deb7f8a5f7003af484079008987d8af
agreene46 <agreene46@arctrdhm002.rs.gsu.edu>
2025-07-09 20:16:45 +0000

    initial ollama testing

--- slurm/job.sh
+++ slurm/job.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #SBATCH -J ollama
-#SBATCH -p qTRDGPU
+#SBATCH -p qTRDGPUH
 #SBATCH -A trends53c17
 #SBATCH -t 00:30:00
 #SBATCH -c 24
 #SBATCH --mem=100g
-#SBATCH --gres=gpu:A40:2
+#SBATCH --gres=gpu:V100:2
 #SBATCH -e err%A-%a.err
 #SBATCH -o out%A-%a.out
+
+# To recognize ollama
+export PATH=/trdapps/linux-x86_64/bin/:$PATH
 
 # Ensure both GPUs are visible to Ollama
 export CUDA_VISIBLE_DEVICES=0,1
@@ -26,3 +29,6 @@ export OLLAMA_BACKEND=gpu
 
 # Run ollama serve
 ollama serve
+
+
+
