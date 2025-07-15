d70b6377358f536485019e9e61d2b36d24327648
nblair7 <nblair7@arctrdagn019.rs.gsu.edu>
2025-07-15 21:39:44 +0000

    Revert "updated output"
    This reverts commit 29c470b5d7f852140cd48c4dc022bd16a9cdde31.

--- slurm/job.sh
+++ slurm/job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -J ollama
-#SBATCH -p qTRDGPUM
+#SBATCH -p qTRDGPUH
 #SBATCH -A trends53c17
 #SBATCH -t 00:30:00
 #SBATCH -c 24
 #SBATCH --mem=100g
-#SBATCH --gres=gpu:A100:8
+#SBATCH --gres=gpu:V100:2
 #SBATCH -e err%A-%a.err
 #SBATCH -o out%A-%a.out
 
