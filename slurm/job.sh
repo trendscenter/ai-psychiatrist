d3eb2f1cb5648397060e1ec0dd7e90dd9afafb0e
agreene46 <agreene46@arctrdhm001.rs.gsu.edu>
2025-07-15 17:45:14 +0000

    Switched gemma with qwq

--- slurm/job.sh
+++ slurm/job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -J ollama
-#SBATCH -p qTRDGPUH
+#SBATCH -p qTRDGPU
 #SBATCH -A trends53c17
 #SBATCH -t 00:30:00
 #SBATCH -c 24
 #SBATCH --mem=100g
-#SBATCH --gres=gpu:V100:2
+#SBATCH --gres=gpu:A40:2
 #SBATCH -e err%A-%a.err
 #SBATCH -o out%A-%a.out
 
